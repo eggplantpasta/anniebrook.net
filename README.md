d9b4a6c01a3e2cdc9e8ac9e70113e69df145148c
Brett Roper <roper.brett@gmail.com>
2013-06-06 23:56:58 +0800

    Total restart. Initial commit.
    Restart based on H5BP. This commit is straight H5BP with css moved to
    scss and compass added.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 anniebrook.net
 ==============
 
-Single page placeholder site for Anniebrook Landscapes.
+Single page site for [Anniebrook Landscapes](http://anniebrook.net).
+
+Based on code from the [HTML5 Boilerplate](https://github.com/h5bp/html5-boilerplate) template, 
+using [Sass](http://sass-lang.com) and [Compass](http://compass-style.org).
--- config.rb
+++ config.rb
@@ -4,15 +4,15 @@
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "img"
 javascripts_dir = "js"
 fonts_dir = "fonts"
 
 output_style = :nested
+environment = :development
 
 relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
 color_output = false
 
